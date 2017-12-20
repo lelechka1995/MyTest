1f3a9fa2079fc0b66288156b3bcb370073caa144
Lena Churak <churak.elenaa@gmail.com>
2017-12-20 13:08:05 +0300

    my first version

---
+++ MyTest/Controllers/Test2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyTest.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        // GET: Test2
+        public ActionResult Test2()
+        {
+            return View();
+        }
+    }
+}
